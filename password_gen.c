6b7af4b066e0cc178a3b7b713e294262a4df7cb8
Rtrdbrah <126190719+VeryProPlayer@users.noreply.github.com>
2026-02-09 21:50:39 +0100

    Update password_gen.c

--- password_gen.c
+++ password_gen.c
@@ -1,1 +1,26 @@
+// libraries needed for this program.
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+void generatepassword(int length) {
+  char charset[] = "abcdefghijkmnopqrstuvwxyzABCDEFGHIJKMNOPQRSTUVWXYZ123456789"
+                   "0!@#$%*;"; //symbols wanted for password
+  char password[length + 1];
+  srand(time(NULL));
+
+  for (int i = 0; i < length; i++) { // For loop 
+    int randomIndex = rand() % (sizeof(charset) - 1);
+    password[i] = charset[randomIndex];
+  }
+  password[length] = '\0'; // Null terminate
+  printf("Password: %s\n", password);
+}
+
+int main() {
+  int len;
+  printf("Enter the length of the password : ");
+  scanf("%d", &len);
+  generatepassword(len);
+  return 0;
+}
